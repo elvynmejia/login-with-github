438ae959eb6e532c1a8aaaa5ca8ea3031bec4987
elvynmejia <elvynmejia@gmial.com>
2014-11-01 02:49:01 -0700

    make changes to user profile, contact and about pages

--- app/models/github_profile.rb
+++ app/models/github_profile.rb
@@ -1,2 +1,15 @@
 class GithubProfile < ActiveRecord::Base
+	  def create_github_profile auth
+  	GithubProfile.create({user_id: id, nickname: auth[:info][:nickname]|| "", 
+  		email: auth[:info][:email]||"", name: auth[:info][:name]||"",
+  		image: auth[:info][:image]||"", 
+  		location: auth[:extra][:raw_info][:location]||"", 
+  		public_repo: auth[:extra][:raw_info][:public_repos]||"",
+  		public_gists: auth[:extra][:raw_info][:public_gists]||"",
+  		followers: auth[:extra][:raw_info][:followers]||"",
+  		following: auth[:extra][:raw_info][:following]||"",
+  		member_since: auth[:extra][:raw_info][:created_at]||"",
+  		access_token: auth[:credentials][:token]
+  		})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,20 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show]
 
   get '/auth/:provider/callback' => 'sessions#create'
+
+  #match 'contact' => 'inquiries#new', via: 'get'
+
+  #match 'sigin', to: 'sessions#new', :as => :signin
   get '/signin' => 'sessions#new', :as => :signin
+
+
+
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
   get '/github' => 'github#index'
+
+
+
+  match 'about', to: 'pages#about', via: 'get'
+  match 'contact', to: 'pages#contact', via: 'get'
 end
